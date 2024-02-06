8aeb97e548c9a54539287d2caa6713ebfbc85467
David3022 <davidafs36@gmail.com>
2024-02-06 19:26:17 -0400

    Update HeroSection.jsx
    Agregado boton de contacto

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,16 +40,16 @@ const HeroSection = () => {
             From Caracas, Venezuela
           </p>
           <div>
-            {/* <Link
+            { <Link 
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
-            </Link> */}
+              Contact
+            </Link> }
             {/* <Link */}
             <a href="/download/Resume.pdf" download className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Descargar CV
+                Download CV
               </span>
             </a>
             {/* </Link> */}
@@ -77,3 +77,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
